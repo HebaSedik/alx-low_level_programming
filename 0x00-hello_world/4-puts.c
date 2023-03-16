172cd036018d5775cc304fe988982620ae310f3a
heba <hebasadik02@gmail.com>
2023-03-16 12:28:46 -0700

    100-intel

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - A program that prints a line with puts function 
  * Return: 0 (Success)
@@ -7,6 +6,6 @@
 
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 return (0);
 }
