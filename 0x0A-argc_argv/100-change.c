03fef47a2b9700e3952de06527ddce869c661fa5
heba <hebasadik02@gmail.com>
2023-04-09 13:11:14 -0700

    100-change.c

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - print minimun number
+ * @argc: number of commandline.
+ * @argv: pointer to array
+ * return: 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argc == 2)
+	{
+		int i, leastcoin = 0, money = atoi(argv[1];
+		int cent[] = {25, 10, 5, 2, 1};
+		for (i = 0; i < 5; i++)
+		{
+		leastcoin += money / cent[i];
+		money = money % cent[i];
+		if (money % cent[i] == 0)
+		{
+		break;
+		}
+		}
+	printf("%d\n", leastcoin);
+	}
+	else
+	{
+	printf("ERROR\n");
+	return (1);
+	}
+	return (0);
+}
